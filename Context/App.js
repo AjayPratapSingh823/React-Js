e28f989ebaf58e24ebb7cf2ac867ad891a12e135
AjayPratapSingh823 <80371996+AjayPratapSingh823@users.noreply.github.com>
2023-01-22 22:40:35 +0530

    Create App.js

---
+++ Context/App.js
@@ -0,0 +1,16 @@
+import React, {component} from 'react'
+import './App.css'
+import ComponentC from './components/ComponentC'
+import {UserProvider} from './components/userContext'
+class App extends Component {
+  render(){
+    reutrn(
+      <div className="App">
+        <UserProvidef value="Vishwas">
+      <ComponentC />
+      </UserProvider>
+      <div>
+      )
+  }
+}
+export default App
