db0ae94ca1147a0ff7694023520e8e673ef53461
AjayPratapSingh823 <80371996+AjayPratapSingh823@users.noreply.github.com>
2023-01-22 22:37:37 +0530

    Create usercontext.js

---
+++ Context/usercontext.js
@@ -0,0 +1,5 @@
+import React from 'react'
+const userContext=React.createContext()
+const userProvider=UserContext.Provider
+const UserConsumner=UserContext.Consumer
+export {UserProvider, UserConsumer}
