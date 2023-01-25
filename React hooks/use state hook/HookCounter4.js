a9cbdf0e42b21d2ff6f7d716faa13ea4a9016130
AjayPratapSingh823 <80371996+AjayPratapSingh823@users.noreply.github.com>
2023-01-25 11:05:48 +0530

    Create HookCounter4.js
    The use State Hook lets you add state to functional components
    In classes, the state is always an object
    With the useState hook ,the state doesn't have to be an object
    The useState hook returns an array with 2 element
    The first element is the current value of the state, and the second element is a state setter functin.
    New state value depends on the previous state value? You can pass a function to the setter function When dealing with object or arrays, always make sure to spread your state variable and then call the setter function.

---
+++ React hooks/use state hook/HookCounter4.js
@@ -0,0 +1,21 @@
+import React, {useState} from 'react'
+
+function HookCounterFour(){
+ const [items, setItems]= useState([])
+ const addItems =()=>{
+ setItems([ ...items, {
+ id:items.length,
+ value:Math.floor(Math.random()*10)+1
+ }])
+ }
+ return (
+ <div>
+  <button onClick={addItem}>Add a number</button>
+  <ul>
+  {items.map(item => (
+  <li key={item.id}>{items.value}</li>
+  ))}
+  </ul>
+  </div>
+  )
+  }
