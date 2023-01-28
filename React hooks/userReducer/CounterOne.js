f3f58526a2062b63c3d14b45cafa4e4707cdd4aa
AjayPratapSingh823 <80371996+AjayPratapSingh823@users.noreply.github.com>
2023-01-28 11:20:26 +0530

    Create CounterOne.js

---
+++ React hooks/userReducer/CounterOne.js
@@ -0,0 +1,6 @@
+/*useReducer is a hook that is used for state management in React 
+userReducer is related to reducer functions
+userReducer(reducer,initialState)
+reducer(currentState,action)*/
+
+ 
